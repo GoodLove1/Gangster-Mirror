5372e3c9dd84cbf657859da46851fd6411102f45
BalaPriyan <110181533+GoodLove1@users.noreply.github.com>
2024-07-28 19:44:26 +0530

    Update users_settings.py

--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -4,8 +4,8 @@ from functools import partial
 from html import escape
 from io import BytesIO
 from os import getcwd
-from pyrogram.filters import command, regex, create
-from pyrogram.handlers import MessageHandler, CallbackQueryHandler
+from hydrogram.filters import command, regex, create
+from hydrogram.handlers import MessageHandler, CallbackQueryHandler
 from time import time
 
 from bot import (
